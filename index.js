7f0559655ddae376b00eace07f8fa083d873c468
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-10-13 21:59:30 +0700

    Install mongoose untuk database mongoDB, upload postingan blog dan image

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+const multer = require('multer');
 const app = express();
 const authRoutes = require('./src/routes/auth');
 const blogRoutes = require('./src/routes/blog');
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + '-'+ file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if( file.mimetype === 'image/png' || 
+        file.mimetype === 'image/jpg' || 
+        file.mimetype === 'image/jpeg'){
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
 app.use(bodyParser.json())
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
 
 // Mengatasi Error CORS
 app.use((req, res, next) => {
@@ -25,6 +47,11 @@ app.use((error, req, res, next) => {
         message: message,
         data: data
     })
-})
+});
 
-app.listen(4000);
+// mongoose.connect('mongodb+srv://ilyas:3KAdNUOmgxMlLZDt@cluster0.rdl2b.mongodb.net/<dbname>?retryWrites=true&w=majority', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/mern-blog', {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        app.listen(4000, () => console.log("Connection Success!"));
+    })
+    .catch(err => console.log(err))
--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,32 +1,45 @@
 const {validationResult} = require('express-validator');
+const BlogPost = require('../models/Blog');
 
 exports.createBlogPost = (req, res, next) => {
-    const {title, body} = req.body;
-    // const image = req.body;
-
+    
     const error = validationResult(req);
-
+    
     if(!error.isEmpty()) {
         const err = new Error("Invalid Value");
         err.errorStatue = 400;
         err.data = error.array()
         throw err
     }
-
-    const result = {
-        message: "Create Blog Success!",
-        data: {
-            post_id: 1,
-            title: title,
-            image: "gambar.jpg",
-            body: body,
-            created_at: "12/10/2020",
-            author: {
-                uid: 1,
-                name: "Nama Author"
-            }
-        }
+    
+    if(!req.file) {
+        const err = new Error("Pilih gambar terlebih dahulu");
+        err.errorStatue = 422;
+        throw err
     }
 
-    res.status(201).json(result);
+    const {title, body} = req.body;
+    const image = req.file.path;
+
+    const Posting = new BlogPost({
+        title: title,
+        body: body,
+        image: image,
+        author: {
+            id: 1,
+            name: "Muhammad Ilyas"
+        }
+    })
+
+    Posting.save()
+        .then(result => {
+            res.status(201).json({
+                message: "Create Blog Success!",
+                data: result
+            });
+        })
+        .catch(err => {
+            console.log('err: ', err)
+        })
+
 }
