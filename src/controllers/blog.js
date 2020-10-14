e190cdc9709659375cd15e57f8c0b48a17e98c10
Muhammad Ilyas <53975283+milyas26@users.noreply.github.com>
2020-10-14 20:08:25 +0700

    Menyelesaikan controller dan route update dan update, menambahkan pagination get blog posts

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,6 +1,10 @@
 const {validationResult} = require('express-validator');
 const BlogPost = require('../models/Blog');
 const { post } = require('../routes/auth');
+const path = require('path');
+const fs = require('fs');
+const Blog = require('../models/Blog');
+const { parse } = require('path');
 
 //CREATE BLOG POST
 exports.createBlogPost = (req, res, next) => {
@@ -16,7 +20,7 @@ exports.createBlogPost = (req, res, next) => {
     
     if(!req.file) {
         const err = new Error("Pilih gambar terlebih dahulu");
-        err.errorStatue = 422;
+        err.errorStatus = 422;
         throw err
     }
 
@@ -48,11 +52,25 @@ exports.createBlogPost = (req, res, next) => {
 
 //READ ALL BLOG POST
 exports.getAllBlogPost = (req, res, next) => {
+    const currentPage = req.query.page || 1;
+    const perPage = req.query.perPage || 5;
+    let totalItems;
+
     BlogPost.find()
+        .countDocuments()
+        .then(count => {
+            totalItems = count;
+            return BlogPost.find()
+                .skip((parseInt(currentPage) - 1) * parseInt(perPage))
+                .limit(parseInt(perPage))
+        })
         .then(result => {
             res.status(200).json({
                 message: "Data Blog Post berhasil dipanggil",
-                data: result
+                data: result,
+                total_data: totalItems,
+                per_page: parseInt(perPage),
+                current_page: parseInt(currentPage)
             })
         })
         .catch(err => {
@@ -67,7 +85,7 @@ exports.getBlogPostById = (req, res, next) => {
         .then(result => {
             if(!result) {
                 const error = new Error(`Data blog dengan id ${postId} tidak ditemukan`)
-                error.errorStatue = 404;
+                error.errorStatus = 404;
                 throw error;
             } else {
                 res.status(200).json({
@@ -79,4 +97,80 @@ exports.getBlogPostById = (req, res, next) => {
         .catch(err => {
             next(err);
         })
+}
+//UPDATE
+exports.updateBlogPost = (req, res, next) => {
+    const error = validationResult(req);
+    
+    if(!error.isEmpty()) {
+        const err = new Error("Invalid Value");
+        err.errorStatus = 400;
+        err.data = error.array()
+        throw err
+    }
+    
+    if(!req.file) {
+        const err = new Error("Pilih gambar terlebih dahulu");
+        err.errorStatus = 422;
+        throw err
+    }
+
+    const {title, body} = req.body;
+    const image = req.file.path;
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+        .then(post => {
+            if(!post) {
+                const err = new Error('Blog post tidak ditemukan!')
+                err.errorStatus = 404;
+                throw err;
+            }
+
+            post.title = title;
+            post.body = body;
+            post.image = image;
+
+            return post.save();
+        })
+        .then(result => {
+            res.status(200).json({
+                message: "Update blog post sukses",
+                data: result
+            })
+        })
+        .catch(err => {
+            next(err);
+        })
+}
+
+//DELETE
+exports.deleteBlogPost = (req, res, next) => {
+    const postId = req.params.postId;
+
+    BlogPost.findById(postId)
+        .then(post => {
+            if(!post) {
+                const error = new Error(`Data blog tidak ditemukan`)
+                error.errorStatus = 404;
+                throw error;
+            }
+
+            removeImage(post.image);
+            return BlogPost.findByIdAndRemove(postId)
+        })
+        .then(result => {
+            res.status(200).json({
+                message: "Hapus berhasil",
+                data: result
+            })
+        })
+        .catch(err => {
+            next(err);
+        })
+}
+
+const removeImage = (filePath) => {
+    filePath = path.join(__dirname, '../..', filePath);
+    fs.unlink(filePath, err => console.log(err));
 }
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -6,13 +6,26 @@ const blogController = require("../controllers/blog");
 //CREATE BLOG POST
 router.post(
   "/post",
-  [body("title").isLength({ min: 5 }).withMessage("Input anda tidak sesuai"), body("body").isLength({ min: 5 }).withMessage("Input anda tidak sesuai")],
-  blogController.createBlogPost
-);
+    [
+      body("title").isLength({ min: 5 }).withMessage("Input anda tidak sesuai"), 
+      body("body").isLength({ min: 5 }).withMessage("Input anda tidak sesuai")
+    ], blogController.createBlogPost);
 
 //READ ALL BLOG POST
 router.get('/posts', blogController.getAllBlogPost)
 
 //READ BLOG POST BY ID
 router.get('/post/:postId', blogController.getBlogPostById)
+
+//UPDATE BLOG POST
+router.put(
+  '/post/:postId', 
+  [
+    body("title").isLength({ min: 5 }).withMessage("Input anda tidak sesuai"), 
+    body("body").isLength({ min: 5 }).withMessage("Input anda tidak sesuai")
+  ], blogController.updateBlogPost)
+  
+//DELETE BLOG POST
+router.delete('/post/:postId', blogController.deleteBlogPost)
+
 module.exports = router;
